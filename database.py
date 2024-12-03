d3fb054d760a061c25bd0755842aee8f42a730d6
mac <referme.manikanta@gmail.com>
2024-12-03 17:16:07 +0530

    open ai search changes

--- database.py
+++ database.py
@@ -12,7 +12,7 @@ def get_sql_connection():
 def get_all_transactions():
     connection = get_sql_connection()
     cursor = connection.cursor(dictionary=True)
-    cursor.execute("SELECT createdOn as date,guid, response, request FROM common_analytics ORDER BY createdOn DESC")
+    cursor.execute("SELECT createdOn as date,guid, response, request,source FROM common_analytics ORDER BY createdOn DESC")
     # cursor.execute(
     #     "SELECT guid, response, request FROM common_analytics WHERE LOWER(guid) = LOWER('0029fe79-4994-4e85-a16b-d0d894850d0c')")
     records = cursor.fetchall()
